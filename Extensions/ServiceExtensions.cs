cebb3124871e0357dc0dcbd6f9d40ce29283b507
Sakibul Islam <sakibul95bubt@gmail.com>
2022-09-05 23:10:39 +0600

    First Commit

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+
+namespace DotnetCore6WebApiTemplate.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+
+        public static void ConfigureForwardHeader(this IServiceCollection services)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+            });
+        }
+
+        public static IServiceCollection ConfigureRepositoryInjections(this IServiceCollection services)
+        {
+            //services.AddTransient<IAccountRepository, AccountRepository>();
+            //services.AddTransient<IQuizmasterRecurringPaymentRepository, QuizmasterRecurringPaymentRepository>();
+
+            return services;
+        }
+
+        //public static void ConfigureJWTAuthentication(this IServiceCollection services, IConfiguration configuration)
+        //{
+        //    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        //    {
+        //        options.RequireHttpsMetadata = false;
+        //        options.SaveToken = true;
+        //        options.TokenValidationParameters = new TokenValidationParameters()
+        //        {
+        //            ValidateIssuer = true,
+        //            ValidateAudience = true,
+        //            ValidAudience = configuration["JWT:ValidAudience"],
+        //            ValidIssuer = configuration["JWT:ValidIssuer"],
+        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+        //        };
+        //    });
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
+using DotnetCore6WebApiTemplate.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.WebHost.UseUrls("https://0.0.0.0:5031");
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.ConfigureForwardHeader();
+
+//builder.Services.AddDbContext<BasketContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BasketDbConnection")));
+//builder.Services.AddDbContext<WapPortalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WapPortalConnection")));
+//builder.Services.AddDbContext<BkashPaymentGatewayContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BkashPaymentGateWayConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.ConfigureRepositoryInjections();
+
+//builder.Services.ConfigureJWTAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,9 +30,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+app.UseStaticFiles();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
